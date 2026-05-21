787d1c8fe0317179d90d4223da27fdceb4b5ed55
Aitanantc <aitanantc@gmail.com>
2026-05-20 19:06:18 -0500

    added empty function to test empty queue changed a bit the description/documentation

--- week03/code/SetsAndMaps.cs
+++ week03/code/SetsAndMaps.cs
@@ -22,7 +22,29 @@ public static class SetsAndMaps
     public static string[] FindPairs(string[] words)
     {
         // TODO Problem 1 - ADD YOUR CODE HERE
-        return [];
+        // var pairs = new List<string>();
+        var pairs = new HashSet<string>();
+        var wordSet = new HashSet<string>(words);
+        foreach (var word in words)
+        {
+            // var reversedWord = new string(word.Reverse().ToArray());
+            // if (wordSet.Contains(reversedWord) && word != reversedWord)
+            char[] charArray = word.ToCharArray();
+            Array.Reverse(charArray);
+            string reversedWord = new string(charArray);
+
+            if (wordSet.Contains(reversedWord) && word != reversedWord)
+            {
+                // pairs.Add($"{word} & {reversedWord}");
+                string pair = string.Compare(word, reversedWord) < 0 ? $"{word} & {reversedWord}" : $"{reversedWord} & {word}";
+                pairs.Add(pair);
+                // Remove the pair from the set to avoid duplicates
+                // wordSet.Remove(word);
+                // wordSet.Remove(reversedWord);
+            }
+        }
+
+        return pairs.ToArray();
     }
 
     /// <summary>
@@ -43,6 +65,15 @@ public static class SetsAndMaps
         {
             var fields = line.Split(",");
             // TODO Problem 2 - ADD YOUR CODE HERE
+                var degree = fields[3].Trim();
+                if (degrees.ContainsKey(degree))
+                {
+                    degrees[degree]++;
+                }
+                else
+                {
+                    degrees[degree] = 1;
+                }
         }
 
         return degrees;
@@ -67,9 +98,59 @@ public static class SetsAndMaps
     public static bool IsAnagram(string word1, string word2)
     {
         // TODO Problem 3 - ADD YOUR CODE HERE
-        return false;
+            // var charCount = new Dictionary<char, int>();
+            var charCount = new Dictionary<char, int>();
+            word1 = word1.Replace(" ", "").ToLower();
+            word2 = word2.Replace(" ", "").ToLower();   
+
+            foreach (var c in word1)
+            {
+                if (charCount.ContainsKey(c))
+                {
+                    charCount[c]++;
+                }
+                else
+                {
+                    charCount[c] = 1;
+                }
+            }
+
+    
+            foreach (char c in word2)
+            {
+                if (!charCount.ContainsKey(c))
+                {
+                    return false;
+                }
+
+                charCount[c]--;
+
+                if (charCount[c] < 0)
+                {
+                    return false;
+                }
+            }
+             foreach (var count in charCount.Values)
+            {
+                if (count != 0)
+                {
+                    return false;
+                }
+            }
+
+            
+        return true;
     }
 
+    // foreach (var value in charcount.values)
+    
+    // {
+        // if (value != 0)
+        // {
+        //     return false;
+        // }           
+        // return true;   
+    // }
     /// <summary>
     /// This function will read JSON (Javascript Object Notation) data from the 
     /// United States Geological Service (USGS) consisting of earthquake data.
@@ -101,6 +182,7 @@ public static class SetsAndMaps
         // on those classes so that the call to Deserialize above works properly.
         // 2. Add code below to create a string out each place a earthquake has happened today and its magitude.
         // 3. Return an array of these string descriptions.
+        
         return [];
     }
 }
--- week03/learn/Translator.cs
+++ week03/learn/Translator.cs
@@ -25,7 +25,9 @@ public class Translator
     public void AddWord(string fromWord, string toWord)
     {
         // ADD YOUR CODE HERE
+        _words[fromWord] = toWord;
     }
+
 
     /// <summary>
     /// Translates the from word into the word that this stores as the translation
@@ -35,6 +37,10 @@ public class Translator
     public string Translate(string fromWord)
     {
         // ADD YOUR CODE HERE
-        return "";
+        if (_words.ContainsKey(fromWord))
+        {
+            return _words[fromWord];
+        } else
+        return "???";
     }
 }
